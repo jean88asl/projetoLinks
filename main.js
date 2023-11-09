e305514bb1bc7557544ab53cceab49224f15d3fb
Jean Augusto <jean88asl@gmail.com>
2023-11-09 15:21:58 -0300

    Alterações finais no projeto.

--- main.js
+++ main.js
@@ -1,14 +1,28 @@
-function toogleMode() {
-    let htmlMode = document.documentElement
+// function toogleMode() {
+//     let htmlMode = document.documentElement
 
-    htmlMode.classList.toggle("light-mode")
+//     htmlMode.classList.toggle("light-mode")
+
+//     let img = document.querySelector('#profile img')
+
+//     // A condição abaixo indica que caso a variável htmlMode contenha a classe light-mode ele troca a imagem para outra
+//     if(htmlMode.classList.contains("light-mode")) {
+//         img.setAttribute('src', './assets/my_avatar_sun.jpg')
+//     }else {
+//         img.setAttribute('src', './assets/my_avatar.png')
+//     }
+// }
+
+function toogleMode() {
+    let mode = document.querySelector("#mode")
+
+    mode.classList.toggle("bg-light")
 
     let img = document.querySelector('#profile img')
-
-    // A condição abaixo indica que caso a variável htmlMode contenha a classe light-mode ele troca a imagem para outra
-    if(htmlMode.classList.contains("light-mode")) {
+    if (mode.classList.contains("bg-light")) {
         img.setAttribute('src', './assets/my_avatar_sun.jpg')
-    }else {
+    } else {
         img.setAttribute('src', './assets/my_avatar.png')
     }
+
 }
